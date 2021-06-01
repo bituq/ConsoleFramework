089182540c5100cdac28385b18cd95fccd95e40a
bituq <d.noorland9@hotmail.com>
2021-06-01 18:53:11 +0200

    Added OnInit action to inputhandler

--- ConsoleFramework/Program.cs
+++ ConsoleFramework/Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleFramework
         {
             var viewport = new Viewport(true);
             var button = new SelectableTextInstance(viewport, "Button", 1, 5, ConsoleColor.Yellow);
+            List<SelectableTextInstance> l = new List<SelectableTextInstance>{
+                new SelectableTextInstance(viewport, "test", 20, 6, ConsoleColor.Red),
+                new SelectableTextInstance(viewport, "test", 10, 8, ConsoleColor.Red),
+                new SelectableTextInstance(viewport, "test", 24, 10, ConsoleColor.Red)
+            };
+            foreach (SelectableTextInstance temp in l)
+                temp.SelectionBackground = ConsoleColor.DarkRed;
             var explanation = new TextInstance(viewport, "Please click the button below.", 1, 3, ConsoleColor.White, ConsoleColor.Black);
             int number = 0;
             var counter = new TextInstance(viewport, $"Counter: {number}", 1, 6, ConsoleColor.DarkGray, ConsoleColor.Black);
@@ -20,8 +27,11 @@ namespace ConsoleFramework
             };
 
             button.SelectionBackground = ConsoleColor.DarkRed;
-            button.Active = true;
 
+            InputHandler.OnInit = () =>
+            {
+                button.Active = true;
+            };
             InputHandler.WaitForInput();
         }
     }
